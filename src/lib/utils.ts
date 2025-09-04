70177bdea62560e9c4be12f692df8113ef88d123
rizkiwnfproject <rizkiwahyuproject@gmail.com>
2025-09-04 11:57:36 +0700

    api route - next auth

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,11 @@ export const hashPassword = async (password: string) => {
 
   return hashedPassword;
 };
+
+export const comparePassword = async (
+  password: string,
+  hashedPassword: string
+) => {
+  const isMatch = await bcrypt.compare(password, hashedPassword);
+  return isMatch;
+};
