cd19194d7d9d28ccd7f7cba2720258b487fa4b15
rizkiwnfproject <rizkiwahyuproject@gmail.com>
2025-09-08 21:44:58 +0700

    fix route company categories

--- src/app/api/company/categories/route.ts
+++ src/app/api/company/categories/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../../lib/prisma";
 
-export async function Get() {
+export async function GET() {
   const categories = await prisma.industry.findMany();
   return NextResponse.json(categories);
 }
